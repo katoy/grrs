5e25af3ffb2967f6e97cef5396710b2a452fb820
katoy <youichikato@gmail.com>
2025-12-10 13:12:43 +0900

    test: 統合テストを追加し、カバレッジを100%に向上
    - src/lib.rsにfind_matchesを移動
    - assert_cmd, assert_fsを使用したCLIテストを追加
    - main.rsの実装をlib.rsの使用に戻し、カバレッジを改善

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+use anyhow::Result;
+
+pub fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line)?;
+        }
+    }
+    Ok(())
+}
---
+++ tests/cli.rs
@@ -0,0 +1,37 @@
+use assert_cmd::Command;
+use assert_fs::prelude::*;
+use grrs::find_matches;
+use predicates::prelude::*; // Used for writing assertions
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result).unwrap();
+    assert_eq!(result, b"lorem ipsum\n");
+}
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::new(assert_cmd::cargo::cargo_bin!("grrs"));
+
+    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::new(assert_cmd::cargo::cargo_bin!("grrs"));
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("A test\nAnother test"));
+
+    Ok(())
+}
